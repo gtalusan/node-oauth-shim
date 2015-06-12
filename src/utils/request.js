f062ec38e4b199877ff004d16890b3b400f09b9b
George Talusan <george.talusan@gmail.com>
2015-06-12 18:50:39 -0400

    hacks for fitbit auth

--- src/utils/request.js
+++ src/utils/request.js
@@ -3,6 +3,22 @@ var http = require('http');
 
 // Wrap HTTP/HTTPS calls
 module.exports = function(req,data,callback){
+        if (req.host === "api.fitbit.com") {
+                var r = url.parse(req.href, true);
+                r.method = req.method;
+                if (!r.query.oauth_token) {
+                        r.headers = {
+                                'Authorization': 'OAuth oauth_consumer_key="' + r.query.oauth_consumer_key + '", oauth_nonce="' + r.query.oauth_nonce + '", oauth_signature="' + encodeURIComponent(r.query.oauth_signature) + '", oauth_signature_method="' + r.query.oauth_signature_method + '", oauth_timestamp="' + r.query.oauth_timestamp + '", oauth_version="' + r.query.oauth_version + '"'
+                        };
+                } else {
+                        r.headers = {
+                                'Authorization': 'OAuth oauth_consumer_key="' + r.query.oauth_consumer_key + '", oauth_nonce="' + r.query.oauth_nonce + '", oauth_signature="' + encodeURIComponent(r.query.oauth_signature) + '", oauth_signature_method="' + r.query.oauth_signature_method + '", oauth_timestamp="' + r.query.oauth_timestamp + '", oauth_token="' + r.query.oauth_token + '", oauth_verifier="' + r.query.oauth_verifier + '", oauth_version="' + r.query.oauth_version + '"'
+                        };
+                }
+                r.query = {};
+                r.path = req.pathname;
+                req = r;
+        }
 	var r = ( req.protocol==='https:' ? https : http ).request( req, function(res){
 		var buffer = '';
 		res.on('data', function(data){
